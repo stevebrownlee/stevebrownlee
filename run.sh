02e3fc0d40fe42ef9dc4286d31bd7060f990dee7
Steve Brownlee <chortlehoort@gmail.com>
2018-02-11 23:44:44 -0600

    Use port 80

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-#docker run -d -it -p 8888:8000 stevebrownlee/api
-docker-compose up
+# docker run -d -it -p 8888:8000 stevebrownlee/api
+# docker-compose up
+docker run -p 80:8000 stevebrownlee/api
 
